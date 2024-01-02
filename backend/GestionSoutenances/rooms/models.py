208b45976033e2a3e0fab987159e7087a22bc936
SEWAH <2ksewah@gmail.com>
2024-01-02 17:31:40 +0000

    Début mise en place des models

--- backend/GestionSoutenances/rooms/models.py
+++ backend/GestionSoutenances/rooms/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class rooms(models.Model):
+    nameRoom = models.CharField(max_length=255)
+    availRoom = models.TextField(max_length=3, default = "yes") #Disponibilité de la salle
+    
+    def __str__(self):
+        return self.nameRoom
+    
