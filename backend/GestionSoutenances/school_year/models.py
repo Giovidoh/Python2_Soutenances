2831eda3690285132e32072852aab20cb496efc8
= <giovidoh@gmail.com>
2024-01-04 16:25:13 +0000

    ajout de student, modification des autres modèles et settings.

--- backend/GestionSoutenances/school_year/models.py
+++ backend/GestionSoutenances/school_year/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 
-class SchoolYear:
-    name = models.CharField(max_length=15)
+class SchoolYear(models.Model):
+    name = models.CharField(max_length = 15)
     
     def __str__(self):
         return(self.name)
