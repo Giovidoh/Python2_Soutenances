8397f2203b7960cf64cbbf3075b81a6ce9a2a785
SEWAH <2ksewah@gmail.com>
2024-01-04 21:34:38 +0000

    Little update of class

--- backend/GestionSoutenances/professors/models.py
+++ backend/GestionSoutenances/professors/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class professors(models.Model):
+class Professors(models.Model):
     name = models.CharField(max_length = 30)
     prenom = models.CharField(max_length = 255)
     contact = models.CharField(max_length = 8)
--- backend/GestionSoutenances/rooms/models.py
+++ backend/GestionSoutenances/rooms/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class rooms(models.Model):
+class Rooms(models.Model):
     name = models.CharField(max_length=255)
     
     def __str__(self):
