df7d14216e41e79e66f98086a5e059bd3d9b014e
SEWAH <2ksewah@gmail.com>
2024-01-04 01:06:06 +0000

    Acces aux tables pour l'administrateur

--- backend/GestionSoutenances/professors/admin.py
+++ backend/GestionSoutenances/professors/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import professors
 
 # Register your models here.
+
+admin.site.register(professors)
--- backend/GestionSoutenances/rooms/admin.py
+++ backend/GestionSoutenances/rooms/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import rooms
 
 # Register your models here.
+
+admin.site.register(rooms)
