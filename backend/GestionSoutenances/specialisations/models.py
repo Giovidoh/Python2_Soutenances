dad2e738df286487c23049e26472a0a41b8c439d
SEWAH <2ksewah@gmail.com>
2024-01-04 22:59:24 +0000

    Creation de la class Specialisations

--- backend/GestionSoutenances/specialisations/models.py
+++ backend/GestionSoutenances/specialisations/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from field_of_study import FieldOfStudy
 
 # Create your models here.
 
 class Specialisations(models.Model):
     name = models.CharField(max_length = 255)
+    field = models.ForeignKey(FieldOfStudy, on_delete=models.CASCADE)
     
     
     def __str__(self):
