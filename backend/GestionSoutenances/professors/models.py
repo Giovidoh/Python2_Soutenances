5e15221a4b656186079f3fc696e6f5d059a9f804
SEWAH <2ksewah@gmail.com>
2024-01-04 00:43:17 +0000

    Fin mise en place des models

--- backend/GestionSoutenances/professors/models.py
+++ backend/GestionSoutenances/professors/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class professors(models.Model):
+    name = models.CharField(max_length = 30)
+    prenom = models.CharField(max_length = 255)
+    contact = models.CharField(max_length = 8)
+    email = models.CharField(max_length = 255)
+    adress = models.CharField(max_length = 255)
+    experience = models.IntegerField(default = 0) #Nombre de soutenances assitées par le professeur
+    
+    def __str__(self):
+        return self.name + self.prenom + self.email
--- backend/GestionSoutenances/rooms/models.py
+++ backend/GestionSoutenances/rooms/models.py
@@ -3,9 +3,8 @@ from django.db import models
 # Create your models here.
 
 class rooms(models.Model):
-    nameRoom = models.CharField(max_length=255)
-    availRoom = models.TextField(max_length=3, default = "yes") #Disponibilité de la salle
+    name = models.CharField(max_length=255)
     
     def __str__(self):
-        return self.nameRoom
+        return self.name
     
