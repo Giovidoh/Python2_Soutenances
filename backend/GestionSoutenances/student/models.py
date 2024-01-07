90376f1699db91c754bc7f56ebbc01f8d1f5f9d3
= <giovidoh@gmail.com>
2024-01-07 21:46:55 +0000

    modification du modèle student.

--- backend/GestionSoutenances/student/models.py
+++ backend/GestionSoutenances/student/models.py
@@ -3,12 +3,18 @@ from django.db import models
 # Create your models here.
 
 class Student(models.Model):
+    serialNumber = models.CharField(max_length = 9, default = None)
     familyName = models.CharField(max_length = 50)
     firstName = models.CharField(max_length = 50)
     gender = models.CharField(max_length = 1)
     birth_date = models.DateField(auto_now_add = False, default = None)
     field_of_study = models.ForeignKey("field_of_study.FieldOfStudy", on_delete = models.CASCADE)
-    school_year = models.ForeignKey("school_year.SchoolYear", on_delete = models.CASCADE)
+    school_year = models.ManyToManyField("school_year.SchoolYear")
+    is_deleted = models.BooleanField(default = False)
     
     def __str__(self):
         return(self.familyName + " " + self.firstName)
+    
+    def soft_delete(self):
+        self.is_deleted = True
+        self.save()
