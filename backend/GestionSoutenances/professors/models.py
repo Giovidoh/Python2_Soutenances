44bf3ca573322d396b18f88ad24f467d2bc0fd74
SEWAH <2ksewah@gmail.com>
2024-01-07 21:49:40 +0000

    CRUD Professors en cours

--- backend/GestionSoutenances/professors/models.py
+++ backend/GestionSoutenances/professors/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
 
@@ -9,6 +10,13 @@ class Professors(models.Model):
     email = models.CharField(max_length = 255)
     address = models.CharField(max_length = 255)
     experience = models.IntegerField(default = 0) #Nombre de soutenances assitées par le professeur
+    is_deleted = models.BooleanField(default = False)
+    date_deleting = models.DateField(null=True, blank=True, default=None)
     
     def __str__(self):
         return self.name + " " + self.firstName + " " + self.email
+    
+    def soft_delete(self):
+        self.is_deleted = True
+        self.date_deleting = datetime.now()
+        self.save()
