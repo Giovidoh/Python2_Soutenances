fc6337d7604ec5690215ac74bd1e85b8e86cc551
SEWAH <2ksewah@gmail.com>
2024-01-07 01:25:47 +0000

    CRUD for App rooms completed successfully

--- backend/GestionSoutenances/rooms/models.py
+++ backend/GestionSoutenances/rooms/models.py
@@ -4,7 +4,11 @@ from django.db import models
 
 class Rooms(models.Model):
     name = models.CharField(max_length=255)
+    is_deleted = models.BooleanField(default = False)
     
     def __str__(self):
         return self.name
     
+    def soft_delete(self):
+        self.is_deleted = True
+        self.save()
--- backend/GestionSoutenances/rooms/views.py
+++ backend/GestionSoutenances/rooms/views.py
@@ -27,24 +27,24 @@ def list(request):
     
     return Response(result)
 
-# Ajout de salle
+# Ajout de nouvelle salle
 @api_view(['POST'])
 def add(request):
     serializer = RoomsSerializer(data=request.data)
     if(serializer.is_valid(raise_exception=True)):
         # Vérifier si la salle existe déjà
-        existing_field = Rooms.objects.filter(name=request.data.get('name')).first()
-        if not existing_field:
+        existing_field = Rooms.objects.filter(name=request.data.get('name'), is_deleted=False).first()
+        if (not existing_field or existing_field.is_deleted == True):
             serializer.save()
-            return Response({'message': 'Salle ajoutée avec succès !', 'Rooms': serializer.data}, status=status.HTTP_201_CREATED)
+            return Response({'message': 'Salle ajoutée avec succès !', 'rooms': serializer.data}, status=status.HTTP_201_CREATED)
         else:
             return Response({'error': 'Cette salle existe déjà ! Veuillez en créer une autre.'})
-
+    
 # Modification de salle
 @api_view(['PUT'])
 def update(request, id):
     rooms = Rooms.objects.filter(id=id).first()
-    if not rooms:
+    if (not rooms or rooms.is_deleted == True):
         return Response({'error': 'Cette salle n\'existe pas.'}, status=status.HTTP_404_NOT_FOUND)
     
     serializer = RoomsSerializer(rooms, data=request.data)
@@ -55,4 +55,16 @@ def update(request, id):
             serializer.save()
             return Response({'message': 'Salle modifiée avec succès !', 'rooms': serializer.data}, status=status.HTTP_200_OK)
         else:
-            return Response({'error': 'Cette salle existe déjà ! Veuillez en créer une autre.'})
+            return Response({'error': 'Cette salle existe déjà ! Veuillez saisir un autre nom.'})
+        
+# Suppression de salle
+@api_view(['DELETE'])
+def delete(request, id):
+    rooms = Rooms.objects.filter(id=id).first()
+    if (not rooms or rooms.is_deleted == True):
+        return Response({'error': 'Cette salle n\'existe pas.'}, status=status.HTTP_404_NOT_FOUND)
+    
+    # Si la salle existe faire une suppression logique
+    rooms.soft_delete()
+    
+    return Response({'message': 'Salle supprimée avec succès'}, status=status.HTTP_200_OK)
