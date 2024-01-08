6a6f230450160d25f5fd190026dd3b555396df94
SEWAH <2ksewah@gmail.com>
2024-01-08 23:52:23 +0000

    Optimisation modification de professeur

--- backend/GestionSoutenances/professors/views.py
+++ backend/GestionSoutenances/professors/views.py
@@ -52,7 +52,7 @@ def update(request, id):
     serializer = ProfessorsSerializer(professors, data=request.data)
     if(serializer.is_valid(raise_exception=True)):
         # Vérifier si le professeur existe déjà
-        existing_field = Professors.objects.filter(name=request.data.get('name'), ).exclude(id=professors.id).first()
+        existing_field = Professors.objects.filter(name=request.data.get('name'), firstName=request.data.get('firstName'), email=request.data.get('email')).exclude(id=professors.id).first()
         if not existing_field:
             serializer.save()
             return Response({'message': 'Professeur modifié avec succès !', 'professors': serializer.data}, status=status.HTTP_200_OK)
