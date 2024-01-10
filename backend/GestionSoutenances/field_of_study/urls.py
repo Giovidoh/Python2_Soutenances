6911a92eab52d2327fa0432ac460aaaaf5ea5c4a
Giovanni <giovidoh@gmail.com>
2024-01-10 01:42:53 +0000

    maj générale des urls (ajout de slashes à la fin).

--- backend/GestionSoutenances/field_of_study/urls.py
+++ backend/GestionSoutenances/field_of_study/urls.py
@@ -5,6 +5,6 @@ from .views import list, add, update, delete
 urlpatterns = [
     path('list/', list, name='list'),
     path('add/', add, name='add'),
-    path('update/<int:id>', update, name='update'),
-    path('delete/<int:id>', delete, name='delete'),
+    path('update/<int:id>/', update, name='update'),
+    path('delete/<int:id>/', delete, name='delete'),
 ]
