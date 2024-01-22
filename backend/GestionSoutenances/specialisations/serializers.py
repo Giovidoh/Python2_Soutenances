502ea1c2b9764c3ab3aeee880ef0936b65c88b37
SEWAH <2ksewah@gmail.com>
2024-01-22 01:51:02 +0000

    Suppression de la spécialisation

--- backend/GestionSoutenances/specialisations/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-
-from .models import Specialisations
-
-class SpecialisationsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Specialisations
-        fields = '__all__'
