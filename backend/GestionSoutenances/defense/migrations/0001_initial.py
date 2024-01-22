be0b7f6e2e47ed977cccfbc69fd593b14afa55f9
= <giovidoh@gmail.com>
2024-01-22 03:39:58 +0000

    maj

--- backend/GestionSoutenances/defense/migrations/0001_initial.py
+++ backend/GestionSoutenances/defense/migrations/0001_initial.py
@@ -15,7 +15,7 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.CreateModel(
+        migrations.CreateModel( 
             name='Defense',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
